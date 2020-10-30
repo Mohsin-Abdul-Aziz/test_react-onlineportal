8be47096fc84c47fbc6198b3517514231eb21034
mohsin <mohsinmazhar1996@gmail.com>
2020-10-30 12:33:18 +1100

    new changes made boy

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose
     .then(()=> console.log('MongoDB connected'))
     .catch(err=> console.log(err))
 
-app.get('/',(req,res)=> res.send('HELLOS'));
+app.get('/',(req,res)=> res.send('Lets try'));
 
 //Use Routes
 app.use('/api/users',users);
