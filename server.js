efae8f8c945af2f8547c102c0a621950037a84e0
mohsin <mohsinmazhar1996@gmail.com>
2020-10-30 12:24:18 +1100

    new changes made

--- server.js
+++ server.js
@@ -35,10 +35,10 @@ const port= process.env.PORT || 5000; //Step 1 for production
 
 
     if (process.env.NODE_ENV === 'production'){
-       app.use(express.static ('../client/build'));
+       app.use(express.static ('client/build'));
 
     app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname,'../client','build','index.html'));
+        res.sendFile(path.join(__dirname,'client','build','index.html'));
     })
  }
 
