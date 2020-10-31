863f8d067152d750e192a715151f5f85f15e05ab
mohsin <mohsinmazhar1996@gmail.com>
2020-10-31 11:28:38 +1100

    new changes made boyy

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose
     .then(()=> console.log('MongoDB connected'))
     .catch(err=> console.log(err))
 
-app.get('/',(req,res)=> res.send('Lets do try'));
+app.get('/',(req,res)=> res.send('Lets do this'));
 
 //Use Routes
 app.use('/api/users',users);
@@ -35,7 +35,9 @@ const port= process.env.PORT || 5000; //Step 1 for production
 
 
     if (process.env.NODE_ENV === 'production'){
-       app.use(express.static ('client/build'));
+       app.use('/static', express.static(path.join(__dirname, 'client/build')));
+
+       //app.use(express.static ('client/build'));
 
     app.get('*',(req,res)=>{
         res.sendFile(path.join(__dirname,'client','build','index.html'));
