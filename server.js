b1058aa695a3f975cec0bfed29ad5968a0595855
mohsin <mohsinmazhar1996@gmail.com>
2020-10-31 12:03:13 +1100

    new changesssss

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose
     .then(()=> console.log('MongoDB connected'))
     .catch(err=> console.log(err))
 
-app.get('/',(req,res)=> res.send('THis timesssssssssssssssssssss'));
+app.get('/get',(req,res)=> res.send('THis timesssssssssssssssssssss'));
 
 //Use Routes
 app.use('/api/users',users);
